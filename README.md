1fc938f818bf556db0cdb8343d5bfe38e87e571d
kuszluk <patryk.jakubovski@gmail.com>
2017-03-20 15:28:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+NewMe
+=====
+
+A Symfony project created on March 19, 2017, 8:02 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // Just render homepage and wait for login/register action
+        return $this->render('index.html.twig');
+    }
+}
